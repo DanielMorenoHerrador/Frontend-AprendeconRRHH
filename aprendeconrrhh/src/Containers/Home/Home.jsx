0f14990851854ed62c7d7465de51cdae5fa5b514
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2022-01-05 17:18:41 +0100

    Añadir archivos a los diferentes containers para las vistas

---
+++ aprendeconrrhh/src/Containers/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
+const Home = () => {
+
+    return (
+        <div>
+            Estas en Home
+        </div>
+    )
+}
+
+export default Home;
