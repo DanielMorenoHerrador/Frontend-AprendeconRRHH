936bf5849b34413da99dec030ddd5cf4aa4d11a7
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2022-01-15 00:58:44 +0100

    Traer los pedidos realizados por el usuario logeado

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -24,23 +24,29 @@ const Profile = (props) => {
         setUsers({ ...user, [e.target.name]: e.target.value });
     }
 
+
+
+    useEffect(() => {
+        getorders();
+        setUsers(props.credentials.user);
+    }, [props.credentials]);
+    
+    useEffect(() => {
+        
+    });
+
     const getorders = async () => {
         const body = {
             userId: user.id,
         };
 
         try {
-            const res = await axios.get(
+            const res = await axios.post(
                 "https://aprendeconrrhh.herokuapp.com/orders/getByUserId",
-                body,
-                token
-            );
-            if (res.data.length === 0) {
-                setmsgError(`NO SE HA ENCONTRADO NINGÚN PEDIDO`);
-            } else {
+                body);
                 setOrders(res.data);
-                console.log(res.data);
-            }
+                console.log(res);
+            
         } catch (error) {
             setmsgError("NO SE HA ENCONTRADO NINGÚN PEDIDO");
         }
@@ -82,44 +88,40 @@ const Profile = (props) => {
         history("/");
     }
 
-    
 
-    useEffect(() => {
-        setUsers(props.credentials.user);
-        getorders();
-    }, [props.credentials]);
 
     if (props.credentials?.token !== '') {
         return (
-            console.log("Estas en perfil"),
-            <div className="designProfileone">
-                <div className="user"><input value={user?.name || false} name="name" onChange={manejaInputs} /></div>
-                <div className="user"><input value={user?.city || false} name="city" onChange={manejaInputs} /></div>
-                <div className="user"><input value={user?.email || false} name="email" onChange={manejaInputs} /></div>
-                <div className="user"><input value={user?.phone || false} name="phone" onChange={manejaInputs} /></div>
-                <div className="user"><input value={user?.adress || false} name="adress" onChange={manejaInputs} /></div>
-                <div className="botoneslogoutupdate">
-                    <div className="sendButton" onClick={() => logOut()}>LOGOUT</div>
-                    <div className="sendButton" onClick={() => update()}>UPDATE</div>
-                </div>
-                <div className="containerorders">
-                    <h2>SERVICIOS CONTRATADOS</h2>
-                    <div className="orders">
-                    {order.map((order) => {
-                return (
-                    <div key={order.id} className="detail">
-                        <h4>Numero de order: {order.id}</h4>
-                        <p>User ID: {order.userId}</p>
-                        <p>Service ID: {order.serviceId}</p>
-                        <p>Fecha de alquiler: {order.rentDate}</p>
+            <><div className="datosperfil">
+                <h2>DATOS DE PERFIL</h2>
+            </div><div className="designProfileone">
+
+                    <div className="user"><input value={user?.name || false} name="name" onChange={manejaInputs} /></div>
+                    <div className="user"><input value={user?.city || false} name="city" onChange={manejaInputs} /></div>
+                    <div className="user"><input value={user?.email || false} name="email" onChange={manejaInputs} /></div>
+                    <div className="user"><input value={user?.phone || false} name="phone" onChange={manejaInputs} /></div>
+                    <div className="user"><input value={user?.adress || false} name="adress" onChange={manejaInputs} /></div>
+                    <div className="botoneslogoutupdate">
+                        <div className="sendButton" onClick={() => logOut()}>LOGOUT</div>
+                        <div className="sendButton" onClick={() => update()}>UPDATE</div>
                     </div>
-                );}
-        )
-        
-    }
+                    <div className="containerorders">
+                        <h2>SERVICIOS CONTRATADOS</h2>
+                        <div className="orders">
+                            {order.map((order) => {
+                                return (
+                                    <div key={order.id} className="detail">
+                                        <h4>Numero de order: {order.id}</h4>
+                                        <p>User ID: {order.userId}</p>
+                                        <p>Service ID: {order.serviceId}</p>
+                                        <p>Fecha de alquiler: {order.rentDate}</p>
+                                    </div>
+                                );
+                            }
+                            )}
+                        </div>
                     </div>
-                </div>
-            </div>
+                </div></>
         )
 
     } else {
